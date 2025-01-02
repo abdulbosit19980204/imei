26ae203d52efb8e08f28fd9b1bc4f5fd5b6a9a6b
abdulbosit19980204 <abdulbosit19980204@gmail.com>
2025-01-02 21:30:02 +0500

    Dashboard design

--- config/urls.py
+++ config/urls.py
@@ -14,6 +14,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework_simplejwt.views import (
@@ -30,3 +32,5 @@ urlpatterns = [
     # path('api/v1/auth/signup/', include('dj_rest_auth.registration.urls')),
     # path('api/v1/auth/', include('dj_rest_auth.urls')),
 ]
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
