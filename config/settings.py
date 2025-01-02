19b1c00ba6cca6a838fb96c9ae7d5848bfc51815
abdulbosit19980204 <abdulbosit19980204@gmail.com>
2025-01-02 21:39:35 +0500

    Dashboard design

--- config/settings.py
+++ config/settings.py
@@ -194,7 +194,7 @@ JAZZMIN_SETTINGS = {
     "site_brand": "IMEI",
 
     # Logo to use for your site, must be present in static files, used for brand on top left
-    "site_logo": "/media/logo.png",
+    # "site_logo": "/media/logo.png",
 
     # Logo to use for your site, must be present in static files, used for login form logo (defaults to site_logo)
     "login_logo": None,
